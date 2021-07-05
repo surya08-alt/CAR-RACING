eb58f5574857633c8c01db1377477e7dfc0c3b71
surya08-alt <77626372+surya08-alt@users.noreply.github.com>
2021-07-05 19:55:18 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,40 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank = null
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+  getranking(){
+    database.ref("RANKING").on("value",(data)=>{this.rank=data.val()})
+  }
+  static updateRANKING(rank){database.ref('/').update({RANKING:rank})}
+}
